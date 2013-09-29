3e80c06a91fef6bc0eee958a73a1d23d408137ed
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2013-09-29 21:38:57 +0200

    Added job scripts and an enabler script for hambach.

---
+++ benchmarks/CSAConnector/enable_csa_hambach.sh
@@ -0,0 +1,2 @@
+. /usr/local/mpi/openmpi/1.4.3/gcc64/bin/mpivars_openmpi-1.4.3_gcc64.sh
+export PYTHONPATH=$HOME/CSA/install/lib/python2.6/site-packages:$HOME/CSA/install/lib64/python2.6/site-packages:$PYTHONPATH
---
+++ benchmarks/CSAConnector/run_bench_runtime_100000_hambach.sh
@@ -0,0 +1,8 @@
+#PBS -l nodes=1:ppn=48
+#PBS -q medium
+#PBS -o /users/eppler/CSA/CSAConnector/${PBS_JOBID}.o
+#PBS -e /users/eppler/CSA/CSAConnector/${PBS_JOBID}.e
+
+. /users/eppler/CSA/CSAConnector/enable.sh
+cd /users/eppler/CSA/CSAConnector
+bash do_bench_runtime_100000.sh
