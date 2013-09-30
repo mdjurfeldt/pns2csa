39442e789785b316d59c72615df9413ff7777692
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2013-10-01 01:14:32 +0200

    Renamed scaling to strong scaling and added data for weak scaling. The plotting scripts still need to be changed accordingly.

---
+++ benchmarks/CSAConnector/do_bench_weak_scaling.sh
@@ -0,0 +1,10 @@
+. /usr/local/mpi/openmpi/1.4.3/gcc64/bin/mpivars_openmpi-1.4.3_gcc64.sh
+
+for np in 1 2 4 6 12 24 48; do
+
+  mpirun -np $np python PyNN_CSAConnector_csa_scaling.py 48000 $np > data/PyNN_CSAConnector_csa_weak_scaling_$np.log
+  # mpirun -np $np python PyNN_CSAConnector_libcsa_scaling.py 48000 $np > data/PyNN_CSAConnector_libcsa_weak_scaling_$np.log
+  mpirun -np $np python nest_CSAConnector_csa_scaling.py 48000 $np > data/nest_CSAConnector_csa_weak_scaling_$np.log
+  mpirun -np $np python nest_CSAConnector_libcsa_scaling.py 48000 $np > data/nest_CSAConnector_libcsa_weak_scaling_$np.log
+
+done
