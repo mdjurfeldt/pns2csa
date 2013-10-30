7828d928bf468a533ec3b71de899b8aa002546e5
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2013-10-30 21:50:55 +0100

    Added benchmark for plain NEST RandomConvergentConnect and added new benchmark scripts to runner scripts.

--- benchmarks/CSAConnector/do_bench_runtime_100000.sh
+++ benchmarks/CSAConnector/do_bench_runtime_100000.sh
@@ -5,5 +5,7 @@ for n in 100000; do
   python PyNN_CSAConnector_libcsa_runtime.py $n > data/PyNN_CSAConnector_libcsa_runtime_$n.log
   python nest_CSAConnector_csa_runtime.py $n > data/nest_CSAConnector_csa_runtime_$n.log
   python nest_CSAConnector_libcsa_runtime.py $n > data/nest_CSAConnector_libcsa_runtime_$n.log
+  python PyNN_FixedProbabilityConnector_runtime.py $n > data/PyNN_FixedProbabilityConnector_runtime_$n.log
+  python nest_RandomConvergentConnect_runtime.py $n > data/nest_RandomConvergentConnect_runtime_$n.log
 
 done
--- benchmarks/CSAConnector/do_bench_strong_scaling.sh
+++ benchmarks/CSAConnector/do_bench_strong_scaling.sh
@@ -5,5 +5,7 @@ for np in 1 2 4 6 12 24 48; do
   mpirun -np $np python PyNN_CSAConnector_libcsa_scaling.py 48000 $np > data/PyNN_CSAConnector_libcsa_strong_scaling_$np.log
   mpirun -np $np python nest_CSAConnector_csa_scaling.py 48000 $np > data/nest_CSAConnector_csa_strong_scaling_$np.log
   mpirun -np $np python nest_CSAConnector_libcsa_scaling.py 48000 $np > data/nest_CSAConnector_libcsa_strong_scaling_$np.log
+  mpirun -np $np python PyNN_FixedProbabilityConnector_scaling.py 48000 $np > data/PyNN_FixedProbabilityConnector_scaling_$np.log
+  mpirun -np $np python nest_RandomConvergentConnect_scaling.py 48000 $np > data/nest_RandomConvergentConnect_scaling_$np.log
 
 done
