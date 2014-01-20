6b92546073e207d6c41597ab74962ea25674114e
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2014-01-21 00:12:20 +0100

    Removed native scaling data and adapted scripts to log and extract weak and strong scaling to separate files. Data will be re-added after re-running the benchmarks.

--- benchmarks/data/extract_data.sh
+++ benchmarks/data/extract_data.sh
@@ -2,4 +2,5 @@ grep -h 'CSAConnector' *runtime_*.log > data_runtime.log
 grep -h 'CSAConnector' *strong_scaling_*.log > data_strong_scaling.log
 grep -h 'CSAConnector' *weak_scaling_*.log > data_weak_scaling.log
 grep -h 'RandomConvergent\|FixedProbability' *runtime_*.log > data_native_runtime.log
-grep -h 'RandomConvergent\|FixedProbability' *scaling_*.log > data_native_scaling.log
+grep -h 'RandomConvergent\|FixedProbability' *weak_scaling_*.log > data_native_weak_scaling.log
+grep -h 'RandomConvergent\|FixedProbability' *strong_scaling_*.log > data_native_strong_scaling.log
